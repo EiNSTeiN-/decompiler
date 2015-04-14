4867a8b8d2b4633a41783233f496b4d3e1ae7bda
Francois Chagnon <francois.chagnon@jadedpixel.com>
2015-04-13 22:35:27 -0400

    travis config

--- tests/run.py
+++ tests/run.py
@@ -4,5 +4,24 @@ import sys
 loader = unittest.TestLoader()
 suite = loader.discover('tests/unit/', pattern='test_*.py')
 
-result = unittest.TextTestRunner(descriptions=True, verbosity=2)
-suite.run(result._makeResult())
+runner = unittest.TextTestRunner(descriptions=True, verbosity=3)
+result = suite.run(runner._makeResult())
+
+if not result.wasSuccessful():
+  print 'not successful'
+
+  for cls, error in result.errors:
+    print '------------------------------'
+    print 'Error:', repr(cls)
+    print '------------------------------'
+    print error
+    print
+
+  for cls, error in result.failures:
+    print '------------------------------'
+    print 'Failure:', repr(cls)
+    print '------------------------------'
+    print error
+    print
+
+  sys.exit(1)
