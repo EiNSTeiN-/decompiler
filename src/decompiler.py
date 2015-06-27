32f94c5213f0ac7d26c22dfa83ce8a3381fb6484
Francois Chagnon <francois.chagnon@jadedpixel.com>
2015-06-27 18:47:53 -0400

    move renamer classes into their own file

--- src/decompiler.py
+++ src/decompiler.py
@@ -4,184 +4,13 @@ import ssa
 import propagator
 from iterators import *
 import pruner
+import renamer
 
 from statements import *
 from expressions import *
 
 import filters.controlflow
 import callconv
-
-class renamer_t(object):
-  """ rename locations """
-
-  def __init__(self, function):
-    self.function = function
-    return
-
-  def rename(self):
-    for op in operand_iterator_t(self.function, filter=self.should_rename):
-      new = self.rename_with(op)
-      op.replace(new)
-      op.unlink()
-    # clear out phi statements with operands that do not have indexes anymore.
-    for phi in operand_iterator_t(self.function, klass=phi_t):
-      for op in list(phi.operands):
-        if op.index is None:
-          op.unlink()
-          phi.remove(op)
-    for stmt in statement_iterator_t(self.function):
-      if isinstance(stmt.expr, assign_t) and isinstance(stmt.expr.op2, phi_t) and len(stmt.expr.op2) == 0:
-        stmt.expr.unlink()
-        stmt.remove()
-    return
-
-class arguments_renamer_t(renamer_t):
-  """ rename arguments """
-
-  def __init__(self, dec):
-    renamer_t.__init__(self, dec.function)
-
-    self.dec = dec
-    self.ssa_tagger = dec.ssa_tagger
-
-    """ keeps track of the next index """
-    self.argn = 0
-
-    """ dict of relations between definition and argument name """
-    self.argument_locations = {}
-    return
-
-  def is_restored(self, expr):
-    for exit, define in self.dec.restored_locations.iteritems():
-      if define == expr and exit != define:
-        return True
-    return False
-
-  def rename_with(self, expr):
-
-    loc = [loc for loc in self.argument_locations.keys() if loc.no_index_eq(expr)]
-    if len(loc) == 1:
-      argn = self.argument_locations[loc[0]]
-    else:
-      argn = self.argn
-      self.argument_locations[expr] = argn
-      self.argn += 1
-
-    name = 'a%u' % (argn, )
-    arg = arg_t(expr.copy(), name)
-    return arg
-
-class register_arguments_renamer_t(arguments_renamer_t):
-
-  def should_rename(self, expr):
-    if not isinstance(expr, regloc_t):
-      return False
-    if self.function.arch.is_stackreg(expr):
-      return False
-
-    if expr in self.ssa_tagger.uninitialized:
-      restored = self.is_restored(expr)
-      if not restored or len(expr.uses) > 0:
-        return True
-
-    if isinstance(expr, assignable_t) and  expr.definition:
-      if expr.definition in self.ssa_tagger.uninitialized:
-        return True
-
-    for loc in self.argument_locations:
-      if loc.no_index_eq(expr):
-        return True
-
-    return False
-
-class stack_arguments_renamer_t(arguments_renamer_t):
-
-  def should_rename(self, expr):
-    if not isinstance(expr, deref_t):
-      return False
-    if not self.function.arch.is_stackvar(expr.op):
-      return False
-    if isinstance(expr.op, sub_t):
-      return False
-
-    if expr in self.ssa_tagger.uninitialized:
-      restored = self.is_restored(expr)
-      if not restored or len(expr.uses) > 0:
-        return True
-
-    if isinstance(expr, assignable_t) and  expr.definition:
-      if expr.definition in self.ssa_tagger.uninitialized:
-        return True
-
-    for loc in self.argument_locations:
-      if loc.no_index_eq(expr):
-        return True
-
-    return False
-
-class stack_variables_renamer_t(renamer_t):
-  """ rename stack locations """
-
-  def __init__(self, function):
-    renamer_t.__init__(self, function)
-
-    """ keeps track of the next index """
-    self.varn = 0
-
-    """ dict of relations between stack location and variable name """
-    self.stack_locations = {}
-    return
-
-  def should_rename(self, expr):
-    if type(expr.parent) is deref_t:
-      return False
-
-    in_phi = type(expr.parent) is phi_t
-    if self.function.arch.is_stackreg(expr) and not expr.is_def:
-      return not in_phi
-    if self.function.arch.is_stackvar(expr):
-      return not in_phi and isinstance(expr, add_t)
-
-    if isinstance(expr, deref_t):
-      if self.function.arch.is_stackreg(expr.op):
-        return True
-      if self.function.arch.is_stackvar(expr.op):
-        return isinstance(expr.op, sub_t)
-
-    return False
-
-  def find_stack_location(self, op):
-    if isinstance(op, deref_t):
-      # continue with just the content of the dereference.
-      op = op[0]
-
-    if self.function.arch.is_stackreg(op):
-      # naked register, like 'esp'
-      return 0
-
-    if isinstance(op, sub_t):
-      # 'esp - 4'
-      return op.op2.value
-
-    assert 'weird stack location?'
-
-  def rename_with(self, op):
-    loc = self.find_stack_location(op)
-
-    var = stack_var_t(loc)
-
-    if loc in self.stack_locations.keys():
-      var_index = self.stack_locations[loc]
-    else:
-      var_index = self.varn
-      self.stack_locations[loc] = var_index
-      self.varn += 1
-    var.name = 's%u' % (var_index, )
-
-    if isinstance(op, deref_t):
-      return var
-
-    return address_t(var)
 
 class function_block_t(object):
   def __init__(self, function, node):
@@ -344,10 +173,10 @@ class step_calls(step_t):
 class step_arguments_renamed(step_t):
   'Arguments are renamed'
   def run(self):
-    r = register_arguments_renamer_t(self.decompiler)
+    r = renamer.register_arguments_renamer_t(self.decompiler)
     r.rename()
 
-    r = stack_arguments_renamer_t(self.decompiler)
+    r = renamer.stack_arguments_renamer_t(self.decompiler)
     r.rename()
 
     self.ssa_tagger.tag_arguments()
@@ -375,7 +204,7 @@ class step_registers_pruned(step_t):
 class step_stack_renamed(step_t):
   'Stack locations and registers are renamed'
   def run(self):
-    r = stack_variables_renamer_t(self.function)
+    r = renamer.stack_variables_renamer_t(self.function)
     r.rename()
 
     self.ssa_tagger.tag_variables()
